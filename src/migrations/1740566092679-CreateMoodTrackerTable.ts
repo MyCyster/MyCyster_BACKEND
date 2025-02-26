f7740650ff481d80d89f5c5052bb8bd49fbf16ac
Success Abhulimen <abhulimensuccess@gmail.com>
2025-02-26 11:54:43 +0100

    feat: add migration file

---
+++ src/migrations/1740566092679-CreateMoodTrackerTable.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateMoodTrackerTable1740566092679 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
