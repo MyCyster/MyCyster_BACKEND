2958b8b057429509978a3790abfc1cb4695aad15
Success Abhulimen <abhulimensuccess@gmail.com>
2025-03-15 15:14:35 +0100

    Reapply "fix: entities"
    This reverts commit 3c49f4642f8dfab49849760e7481fc0401cbbb0b.

--- src/typeorm-cli-config.ts
+++ src/typeorm-cli-config.ts
@@ -2,6 +2,8 @@ import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 import { Mood } from './mood-tracker/entities/mood.entity';
 import { Users } from './user/entities/user.entity';
+import { MealPlan } from './meal-planner/entities/meal-planner.entity';
+import { Food } from './meal-planner/entities/food.entity';
 
 dotenv.config();
 
@@ -12,13 +14,13 @@ export default new DataSource({
   database: process.env.DB_NAME,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  entities: [Mood, Users],
+  entities: [Mood, Users, MealPlan, Food],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
   synchronize: false,
   extra: {
     charset: 'utf8mb4_unicode_ci',
   },
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
